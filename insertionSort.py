ce1bcfaf44ae6e90f0ed3ac1fe77e33d98177f04
NadzGit <nadineseddat@gmail.com>
2026-01-10 01:40:01 +0000

    also worked on insertion sort

--- insertionSort.py
+++ insertionSort.py
@@ -1,13 +1,27 @@
-def insertionSortC(A):
-    B = A[:]
+# insertion sort
+# the correct way to go about this is traversing an array BACKWARDS.
+# compare value (v) you want to insert with the already sorted elements on the LEFT
+# shift any elements that are larger than v to the RIGHT
+# by traversing backwards, we are able to avoid any overwriting
+# how? see below.
 
-    for i in range (1, len(B)):
-        j = i
-        while j > 0 and B[j-1] > B[j]:
-            B[j-1], B[j] = B[j], B[j-1]
-            j -=1
+# (we skipped the first couple sorts)
+# 1,3,65|21,3,1 <-- here, 21 is less than 65.
+# 1,3,65,65,|3,1 <-- we shift 65 to the right. it may appear that we have lost the value of 21, but it is saved in a variable passed to the function.
+# 1,3,21,65,|3,1 <-- 21 >= 3, so this is where we place 21.
 
-        j += 1
+def insertionSort(A):
+    for i in range(1,len(A)):
+        insert(A[i],A,i)
 
-    
-    return B
+def insert(v, A, hi):
+    for i in range (hi-1,-1,-1):
+        if v >= A[i]:
+            A[i+1] = v
+            return
+        A[i+1] = A[i]
+    A[0] = v
+
+A = [1,3,3,7,5]  
+insertionSort(A)
+print(A)
